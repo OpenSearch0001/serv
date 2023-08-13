df0042811315edc7dac14ddee31ac8ce69a5bd38
OpenSearch0001 <142180265+OpenSearch0001@users.noreply.github.com>
2023-08-13 16:37:53 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
+const http = require("http");
+const WebSocket = require("ws");
 
+const server = http.createServer();
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", socket => {
+    console.log("A user connected");
+
+    socket.on("message", message => {
+        wss.clients.forEach(client => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(message.toString()); // Convert the message to a string
+            }
+        });
+    });
+
+    socket.on("close", () => {
+        console.log("A user disconnected");
+    });
+});
+
+server.listen(3000, () => {
+    console.log("WebSocket server is running on port 3000");
+});
